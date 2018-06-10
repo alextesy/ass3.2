abae04f42d5e18baef97c74445c149b060f174f4
idanrazy <idan.razy@gmail.com>
2018-06-11 00:06:33 +0300

    comit

--- client/components/homeCtrl.js
+++ client/components/homeCtrl.js
@@ -3,9 +3,14 @@ angular.module('poiApp')
     let serverUrl='http://localhost:3000/'
     self=this;
     $scope.user={};
+    $scope.selcetedcat = {};
     $scope.submit=function(){
         var username=$scope.user.username;
         var password=$scope.user.password;
+        var selcetedcat = $scope.categories
+        console.log(selcetedcat)
+        
+     
     };
 
         $http.get(serverUrl + "POI/allCategories")
